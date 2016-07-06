03f784dcd7396ad2d602cec214a3f226fe6f32a6
felix <felix@netopia.ro>
2016-07-06 21:36:31 +0300

    added where clause based on query string

--- data.js
+++ data.js
@@ -4,7 +4,15 @@ module.exports = function(app,cli){
     app.get('/data/:keyspace/:table', function(req, res){
         console.log('get data for',req.params.keyspace, req.params.table)
         if(req.params.keyspace != null && req.params.table != null) {
-            var q = "select * from "+req.params.keyspace+"."+req.params.table+";"
+            var q = "select * from "+req.params.keyspace+"."+req.params.table
+	        if( JSON.stringify(req.query) !== JSON.stringify({})) {
+		        console.log(JSON.stringify(req.query))
+                q += " WHERE "
+	    	    for(k in req.query) {
+			        q += k + "=" + req.query[k] + "" 
+		        }
+	        }
+	        q += "  ;"
             console.log("executing",q)
             cli.execute(q,function(err,resp){
                 if(err){
